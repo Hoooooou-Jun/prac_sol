bb8c5159f6d2a87b2561dc4ef9b9e8ff148db054
Hoooooou-Jun <jipkim2@gmail.com>
2023-11-22 18:49:59 +0900

    boilerplate

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -21,21 +21,3 @@ pub fn process_instruction(
     // exit program
     Ok(())
 }
-
-
-
-/* Test code example */
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
